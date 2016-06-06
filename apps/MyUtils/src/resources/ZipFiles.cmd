0bff0ac0ef991e40dacf6cd78fe263f264f53a05
ghyssee <eric.ghyssens@hotmail.nl>
2016-06-06 13:56:40 +0200

    removed obsolete code

--- apps/MyUtils/src/resources/ZipFiles.cmd
+++ apps/MyUtils/src/resources/ZipFiles.cmd
@@ -1,10 +1,1 @@
-@echo off
-FOR /F "tokens=2-4 delims=/-/ " %%A IN ("%date%") DO (
-SET DAY=%%A
-SET MONTH=%%B
-SET YEAR=%%C
-)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set MYTIME=%%a:%%b)
-
-java -Xmx1024m -cp MyUtils-1.0.jar be.home.main.ZipFiles %1 %2 %3 %4.%date%
-pause
+java -Xmx1024m -cp MyUtils-1.0.jar be.home.main.ZipFiles %1 %2 %3 %4
