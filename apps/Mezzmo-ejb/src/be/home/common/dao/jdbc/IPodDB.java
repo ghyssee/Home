f5c1098373b271d7fb6254a8b412628c7022de94
ghyssee <eric.ghyssens@hotmail.nl>
2016-09-27 13:32:29 +0200

    Added Batch SynchronizeIPodPlayCount

---
+++ apps/Mezzmo-ejb/src/be/home/common/dao/jdbc/IPodDB.java
@@ -0,0 +1,7 @@
+package be.home.common.dao.jdbc;
+
+/**
+ * Created by ghyssee on 27/09/2016.
+ */
+public class IPodDB {
+}
---
+++ apps/MyUtils/src/java/be/home/main/mezzmo/SynchronizeIPodPlayCount.java
@@ -0,0 +1,7 @@
+package be.home.main.mezzmo;
+
+/**
+ * Created by ghyssee on 27/09/2016.
+ */
+public class SynchronizeIPodPlayCount {
+}
