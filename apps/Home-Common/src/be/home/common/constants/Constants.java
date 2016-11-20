3b30086476c01f8b0512201de86e39051c0e68b5
ghyssee <eric.ghyssens@hotmail.nl>
2016-11-20 19:57:45 +0100

    cleanup of constants

--- apps/Home-Common/src/be/home/common/constants/Constants.java
+++ apps/Home-Common/src/be/home/common/constants/Constants.java
@@ -11,9 +11,9 @@ import java.nio.file.FileSystem;
  */
 public interface Constants {
 
-    public interface Movies {
+    interface Movies {
 
-        public enum Import {
+        enum Import {
             ID(0), TITLE(1), ALTERNATE_TITLE(2), YEAR(3), GENRE(4), STOCKPLACE(5), IMDB_ID(6);
             private int value;
 
@@ -26,31 +26,31 @@ public interface Constants {
             }
         }
 
-        public enum Status {
+        enum Status {
             NFO, SOURCE, DESTINATION, ERROR_NFO, DIFFERENT_TITLE, EXIST_NFO;
         }
     }
 
-    public interface Path {
-        public static final String BASEDIR = "mp3Processor";
-        public static final String ALBUM = "album";
-        public static final String TMP = "tmp";
-        public static final String TMP_JAVA = "tmpjava";
-        public static final String PREPROCESS = "preprocess";
-        public static final String PROCESS = "process";
-        public static final String NEW = "new";
-        public static final String ONEDRIVE = "oneDrive";
-        public static final String CONFIG = "config";
-        public static final String DATA = "data";
-        public static final String PLAYLIST = "playlist";
-        public static final String LOCAL_CONFIG = "localConfig";
-        public static final String VELOCITY = "velocity";
-        public static final String RESOURCES = "resource";
-        public static final String MAIN_LOG = "mainLog";
-        public static final String LOG = "log";
-        public static final String WEB = "web";
-        public static final String WEB_MUSIC = "webMusic";
-        public static final String WEB_MUSIC_ALBUMS = "webMusicAlbums";
-        public static final String WEB_MUSIC_SONGS = "webMusicSongs";
+    interface Path {
+        String BASEDIR = "mp3Processor";
+        String ALBUM = "album";
+        String TMP = "tmp";
+        String TMP_JAVA = "tmpjava";
+        String PREPROCESS = "preprocess";
+        String PROCESS = "process";
+        String NEW = "new";
+        String ONEDRIVE = "oneDrive";
+        String CONFIG = "config";
+        String DATA = "data";
+        String PLAYLIST = "playlist";
+        String LOCAL_CONFIG = "localConfig";
+        String VELOCITY = "velocity";
+        String RESOURCES = "resource";
+        String MAIN_LOG = "mainLog";
+        String LOG = "log";
+        String WEB = "web";
+        String WEB_MUSIC = "webMusic";
+        String WEB_MUSIC_ALBUMS = "webMusicAlbums";
+        String WEB_MUSIC_SONGS = "webMusicSongs";
     }
 }
