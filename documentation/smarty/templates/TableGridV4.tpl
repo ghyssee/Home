f7ba06949e9d88e5576022dd8c9c49a5e20d7581
ghyssee <eric.ghyssens@hotmail.nl>
2017-01-09 15:51:10 +0100

    added suport for editable datagrid

---
+++ documentation/smarty/templates/TableGridV4.tpl
@@ -0,0 +1,152 @@
+	<table id="dg" title="{{$title}}" class="easyui-datagrid" style="width:{{$tableWidth}};height:{{$tableHeight}}" idField="id"
+			url="{{$viewUrl}}"
+			toolbar="#toolbar" pagination="true" nowrap="false" rownumbers="true" fitColumns="true" singleSelect="true">
+		<thead>
+		<tr>
+			{{if isset($checkbox)}}
+				<th field="{{$checkboxField}}" checkbox="true"></th>
+			{{/if}}
+			{{section name=sec1 loop=$contacts}}
+            <th field='productid' title='Product' width="100" formatter="getName()" 
+                editor= "type:'combobox',
+                    options:{
+                        valueField:'productid',
+                        textField:'name',
+                        data:products,
+                        required:true
+                    }"
+            >
+				
+			<th field="{{$contacts[sec1].field}}" {{if isset($contacts[sec1].hidden) && $contacts[sec1].hidden}}hidden="true"{{/if}}
+				width="{{$contacts[sec1].size}}"
+				{{if isset($contacts[sec1].formatter)}} formatter="{{$contacts[sec1].formatter}}"{{/if}}
+                {{if isset($contacts[sec1].sortable)}} sortable={{if $contacts[sec1].sortable}}"true"{{else}}"false"{{/if}}{{/if}}
+            >
+                {{$contacts[sec1].label}}
+			</th>
+			{{/section}}
+		</tr>
+		</thead>
+	</table>
+
+	<span style="font-size:20px">
+	<div id="toolbar">
+	{{if isset($newUrl)}}
+		<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="newRecord()">New {{$item}}</a>
+	{{/if}}
+	{{if isset($updateUrl)}}
+		<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editRecord()">Edit {{$item}}</a>
+	{{/if}}
+	{{if isset($deleteUrl)}}
+		<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="deleteRecord()">Remove {{$item}}</a>
+	{{/if}}
+	</div>
+	</span>
+
+	<div id="dlg" class="easyui-dialog" style="width:400px;height:280px;padding:10px 20px"
+			closed="true" buttons="#dlg-buttons">
+		<div class="ftitle">{{$item}} Information</div>
+		<form id="fm" method="post" novalidate>
+		
+			{{section name=sec1 loop=$contacts}}
+			  {{if !isset($contacts[sec1].hidden) OR !$contacts[sec1].hidden}}
+				<div class="fitem">
+					<label>{{$contacts[sec1].label}}</label>
+					<input name="{{$contacts[sec1].field}}" class="easyui-textbox" required="true">
+				</div>
+				{{/if}}
+			{{/section}}
+
+		</form>
+	</div>
+	<div id="dlg-buttons">
+		<a href="javascript:void(0)" class="easyui-linkbutton c6" iconCls="icon-ok" onclick="save()" style="width:90px">Save</a>
+		<a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="javascript:$('#dlg').dialog('close')" style="width:90px">Cancel</a>
+	</div>
+
+
+	<script type="text/javascript">
+		$('#dg').datagrid({
+			options:[[
+				{rowHeight:100}
+			]],
+			autoRowHeight:true
+		});
+		var url;
+		function newRecord(){
+			$('#dlg').dialog('open').dialog('setTitle','New {{$item}}');
+			$('#fm').form('clear');
+			url = {{$newUrl}};
+		}
+		function editRecord(){
+			var row = $('#dg').datagrid('getSelected');
+			if (row){
+				$('#dlg').dialog('open').dialog('setTitle','Edit {{$item}}');
+				$('#fm').form('load',row);
+				url = {{$updateUrl}};
+			}
+		}
+		function save(){
+			$('#fm').form('submit',{
+				url: url,
+				onSubmit: function(){
+					return $(this).form('validate');
+				},
+				success: function(result){
+                    var result = eval('('+result+')');
+					if (result.errorMsg){
+						$.messager.show({
+							title: 'Error',
+							msg: result.errorMsg
+						});
+					} else {
+						$('#dlg').dialog('close');		// close the dialog
+						$('#dg').datagrid('reload');	// reload the user data
+					}
+				}
+			});
+		}
+		function deleteRecord(){
+			var row = $('#dg').datagrid('getSelected');
+			if (row){
+				$.messager.confirm('Confirm','Are you sure you want to delete this {{$item}}?',function(r){
+					if (r){
+						$.post({{$deleteUrl}},function(result){
+                            if (result.success){
+								$('#dg').datagrid('reload');	// reload the user data
+							} else {
+								$.messager.show({	// show error message
+									title: 'Error',
+									msg: result.errorMsg
+								});
+							}
+						},'json');
+					}
+				});
+			}
+		}
+	</script>
+
+	<style type="text/css">
+		#fm{
+			margin:0;
+			padding:10px 30px;
+		}
+		.ftitle{
+			font-size:14px;
+			font-weight:bold;
+			padding:5px 0;
+			margin-bottom:10px;
+			border-bottom:1px solid #ccc;
+		}
+		.fitem{
+			margin-bottom:5px;
+		}
+		.fitem label{
+			display:inline-block;
+			width:80px;
+		}
+		.fitem input{
+			width:160px;
+		}
+	</style>
--- documentation/web/apps/php/configuration/MP3PreprocessorView.php
+++ documentation/web/apps/php/configuration/MP3PreprocessorView.php
@@ -86,6 +86,128 @@ $smarty->assign("contacts", array(array("field" => "id", "label"=>"Id", "size" =
 $smarty->display('TableGridV3.tpl');
 ?>
 
+<table id="tt"></table>
+
+
+<script>
+    var products = [
+        {productid:'FI-SW-01',name:'Koi'},
+        {productid:'K9-DL-01',name:'Dalmation'},
+        {productid:'RP-SN-01',name:'Rattlesnake'},
+        {productid:'RP-LI-02',name:'Iguana'},
+        {productid:'FL-DSH-01',name:'Manx'},
+        {productid:'FL-DLH-02',name:'Persian'},
+        {productid:'AV-CB-01',name:'Amazon Parrot'}
+    ];
+    $(function(){
+        $('#tt').datagrid({
+            title:'Editable DataGrid',
+            iconCls:'icon-edit',
+            width:660,
+            height:250,
+            singleSelect:true,
+            idField:'itemid',
+            url:'data/datagrid_data.json',
+            columns:[[
+                {field:'itemid',title:'Item ID',width:60},
+                {field:'productid',title:'Product',width:100,
+                    formatter:function(value,row){
+                        return row.productname || value;
+                    },
+                    editor:{
+                        type:'combobox',
+                        options:{
+                            valueField:'productid',
+                            textField:'name',
+                            data:products,
+                            required:true
+                        }
+                    }
+                },
+                {field:'listprice',title:'List Price',width:80,align:'right',editor:{type:'numberbox',options:{precision:1}}},
+                {field:'unitcost',title:'Unit Cost',width:80,align:'right',editor:'numberbox'},
+                {field:'attr1',title:'Attribute',width:180,editor:'text'},
+                {field:'status',title:'Status',width:50,align:'center',
+                    editor:{
+                        type:'checkbox',
+                        options:{
+                            on: 'P',
+                            off: ''
+                        }
+                    }
+                },
+                {field:'action',title:'Action',width:80,align:'center',
+                    formatter:function(value,row,index){
+                        if (row.editing){
+                            var s = '<a href="javascript:void(0)" onclick="saverow(this)">Save</a> ';
+                            var c = '<a href="javascript:void(0)" onclick="cancelrow(this)">Cancel</a>';
+                            return s+c;
+                        } else {
+                            var e = '<a href="javascript:void(0)" onclick="editrow(this)">Edit</a> ';
+                            var d = '<a href="javascript:void(0)" onclick="deleterow(this)">Delete</a>';
+                            return e+d;
+                        }
+                    }
+                }
+            ]],
+            onEndEdit:function(index,row){
+                var ed = $(this).datagrid('getEditor', {
+                    index: index,
+                    field: 'productid'
+                });
+                row.productname = $(ed.target).combobox('getText');
+            },
+            onBeforeEdit:function(index,row){
+                row.editing = true;
+                $(this).datagrid('refreshRow', index);
+            },
+            onAfterEdit:function(index,row){
+                row.editing = false;
+                $(this).datagrid('refreshRow', index);
+            },
+            onCancelEdit:function(index,row){
+                row.editing = false;
+                $(this).datagrid('refreshRow', index);
+            }
+        });
+    });
+    function getRowIndex(target){
+        var tr = $(target).closest('tr.datagrid-row');
+        return parseInt(tr.attr('datagrid-row-index'));
+    }
+    function editrow(target){
+        $('#tt').datagrid('beginEdit', getRowIndex(target));
+    }
+    function deleterow(target){
+        $.messager.confirm('Confirm','Are you sure?',function(r){
+            if (r){
+                $('#tt').datagrid('deleteRow', getRowIndex(target));
+            }
+        });
+    }
+    function saverow(target){
+        $('#tt').datagrid('endEdit', getRowIndex(target));
+    }
+    function cancelrow(target){
+        $('#tt').datagrid('cancelEdit', getRowIndex(target));
+    }
+    function insert(){
+        var row = $('#tt').datagrid('getSelected');
+        if (row){
+            var index = $('#tt').datagrid('getRowIndex', row);
+        } else {
+            index = 0;
+        }
+        $('#tt').datagrid('insertRow', {
+            index: index,
+            row:{
+                status:'P'
+            }
+        });
+        $('#tt').datagrid('selectRow',index);
+        $('#tt').datagrid('beginEdit',index);
+    }
+</script>
 
 <div class="emptySpace"></div>
 <br><br>
