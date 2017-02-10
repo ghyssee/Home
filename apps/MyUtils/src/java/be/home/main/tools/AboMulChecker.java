c17a7ccacbe0e3ec007eafa695da950b2a90da9e
ghyssee <eric.ghyssens@hotmail.nl>
2017-02-10 15:53:56 +0100

    Moved Filename replacements to the MP3Helper class
    Added new Screen for adding filename replacements
    added priority to the Word Class (important for file replacements)

---
+++ apps/MyUtils/src/java/be/home/main/tools/AboMulChecker.java
@@ -0,0 +1,7 @@
+package be.home.main.tools;
+
+/**
+ * Created by ghyssee on 10/02/2017.
+ */
+public class AboMulChecker {
+}
---
+++ documentation/web/apps/php/configuration/MP3PrettifierViewFilename.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ghyssee
+ * Date: 10/02/2017
+ * Time: 13:40
+ */
