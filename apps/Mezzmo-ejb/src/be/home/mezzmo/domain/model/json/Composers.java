edfe964aedb6175bc699b70a9ada962fbcf9cc85
ghyssee <eric.ghyssens@hotmail.nl>
2023-05-24 13:05:11 +0200

    Made abstract class Pattern

--- apps/Mezzmo-ejb/src/be/home/mezzmo/domain/model/json/Composers.java
+++ apps/Mezzmo-ejb/src/be/home/mezzmo/domain/model/json/Composers.java
@@ -7,13 +7,7 @@ public class Composers {
         public List<be.home.mezzmo.domain.model.json.Composers.Composer> composers;
     public List<be.home.mezzmo.domain.model.json.Composers.Publisher> publishers;
 
-        public class Composer {
-
-            public Composer (String id, String pattern){
-                this.id = id;
-                this.pattern = pattern;
-            }
-
+    public abstract class Pattern {
             public String id;
             public String pattern;
 
@@ -34,29 +28,17 @@ public class Composers {
             }
 
         }
-    public class Publisher {
+
+        public class Composer extends Pattern {
+            public Composer (String id, String pattern){
+                this.id = id;
+                this.pattern = pattern;
+            }       }
+
+    public class Publisher extends Pattern {
 
         public Publisher (String id, String pattern){
             this.id = id;
-            this.pattern = pattern;
-        }
-
-        public String id;
-        public String pattern;
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getPattern() {
-            return pattern;
-        }
-
-        public void setPattern(String pattern) {
             this.pattern = pattern;
         }
 
